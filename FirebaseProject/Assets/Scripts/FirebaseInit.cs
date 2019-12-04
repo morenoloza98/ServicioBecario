05c1ca0ac2b03eb8c007387df2606ea41f52b8a8
morenoloza98 <morenolozalex98@gmail.com>
2019-12-04 11:20:08 -0600

    Init commit

---
+++ FirebaseProject/Assets/Scripts/FirebaseInit.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Firebase;
+using Firebase.Analytics;  
+
+public class FirebaseInit : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        FirebaseApp.CheckAndFixDependenciesAsync().ContinueWith(task =>
+        {
+            FirebaseAnalytics.SetAnalyticsCollectionEnabled(true);
+        });
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
